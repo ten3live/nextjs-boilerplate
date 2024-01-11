f2b32ded7d8184a315373c6db4248e30ddeb5a3c
Sajjad Ali <ten3live@gmail.com>
2024-01-11 13:53:21 -0800

    Fixed

--- app/blog/page.js
+++ app/blog/page.js
@@ -24,7 +24,7 @@ const tabs = [
       content: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     }
   ];
-export const CustomRadio = (props) => {
+const CustomRadio = (props) => {
 	const {
 	  Component,
 	  children,
